40d66c4a23366b2d0ae1924db6453afd5dc4999e
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-05 22:43:33 +0100

    Add handling the login process to app.js

--- client/app.js
+++ client/app.js
@@ -6,3 +6,19 @@ const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
+
+loginForm.addEventListener('submit', (event) => {
+  login(event);
+});
+
+const login = event => {
+  event.preventDefault();
+  console.log('userNameInput.value:', userNameInput.value);
+  if (!userNameInput.value) {
+    alert('You cannot leave username empty!')
+  } else {
+    userName = userNameInput;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  };
+};
